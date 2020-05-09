daa45ecec35962b38d8d1715810671fae0623222
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-05-09 21:53:14 +0200

    Implemented SSR for multiple linear regression

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -14,3 +14,14 @@ class MultipleLinearRegression():
     def r2_score(self, y_true, y_pred):
         pass
 
+    def _estimate_coefficients(self, x, y):
+        '''
+            β = (X^T X)^-1 X^T y
+        '''
+        xT = x.transpose()
+
+        inversed = np.linalg.inv( xT.dot(x) )
+        coefficients = inversed.dot( xT ).dot(y)
+
+        return coefficients
+
